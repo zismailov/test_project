6eb96fb8bdec5b83266e1d58361a90d127178c46
Zulkar <bedelbaev@gmail.com>
2025-05-21 23:29:53 +0600

    feat: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Очищаем базу данных..."
+Foo.destroy_all
+
+puts "Создаем тестовые данные..."
+
+5.times do |i|
+  foo = Foo.create!(
+    name: "Foo #{i + 1}"
+  )
+
+  rand(3..5).times do |j|
+    foo.bars.create!(
+      name: "Bar #{j + 1} для Foo #{i + 1}",
+      description: "Описание Bar #{j + 1} для Foo #{i + 1}"
+    )
+  end
+
+  puts "Создан Foo #{i + 1} с #{foo.bars.count} Bar"
+end
+
+puts "- #{Foo.count} Foo"
+puts "- #{Bar.count} Bar"
